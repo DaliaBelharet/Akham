c23a07b6056374e7806464b9494a594f05e3b68c
DaliaBelharet <daliabelharett@gmail.com>
2024-06-17 18:47:42 +0200

    17-06

--- client/src/pages/SearchResults.jsx
+++ client/src/pages/SearchResults.jsx
@@ -1,24 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { FaLocationDot } from 'react-icons/fa6';
 import { Link, useLocation } from 'react-router-dom';
-import sorry from '../assets/sorry.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import sorry from '../assets/sorry.png';
+import Navbar from '../components/Navbar1';
+import Footer from '../components/footer';
 
 const SearchResults = () => {
   const location = useLocation();
-  const { houses } = location.state || { houses: [] };
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fakeLoading = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => {
-      clearTimeout(fakeLoading);
-    };
-  }, []);
+  const { houses, loading } = location.state || { houses: [], loading: false };
 
   if (loading) {
     return (
@@ -31,41 +22,59 @@ const SearchResults = () => {
 
   if (!Array.isArray(houses) || houses.length === 0) {
     return (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f9f9f9' }}>
-        <div style={{ display: 'flex', alignItems: 'center', maxWidth: '800px', padding: '20px', boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)', borderRadius: '10px', background: 'white' }}>
-          <img src={sorry}  style={{ width: '300px', height: 'auto', marginRight: '20px' }} />
-          <div>
-            <p style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '10px' }}>Désolé pour le moment, il n'y a pas de biens correspondant à vos critères de recherche.</p>
-            <p>Revenez souvent pour découvrir de nouvelles annonces !</p>
-            <p>Modifiez vos critères de recherche pour obtenir plus de résultats.</p>
-            <Link to="/home" style={{ color: '#F27438', textDecoration: 'none', fontWeight: 'bold' }}>Retour à la page d'accueil</Link>
+      <div>
+        <Navbar />
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh', backgroundColor: '#f0f0f0' }}>
+          <div style={{ display: 'flex', alignItems: 'center', maxWidth: '800px', padding: '20px', boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)', borderRadius: '10px', background: 'white' }}>
+            <img src={sorry} style={{ width: '300px', height: 'auto', marginRight: '20px' }} alt="Sorry" />
+            <div>
+              <p style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '10px' }}>Désolé pour le moment, il n'y a pas de biens correspondant à vos critères de recherche.</p>
+              <p>Revenez souvent pour découvrir de nouvelles annonces !</p>
+              <p>Modifiez vos critères de recherche pour obtenir plus de résultats.</p>
+              <Link to="/home" style={{ color: '#F27438', textDecoration: 'none', fontWeight: 'bold' }}>Retour à la page d'accueil</Link>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
     );
   }
-  
+
   return (
-    <div style={{ fontFamily: "Arial, sans-serif" }}>
-      <h1 style={{ fontSize: "36px", fontWeight: "bold", textAlign: "center", marginBottom: "40px" }}>Résultats de la recherche</h1>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexWrap: "wrap" }}>
-        {houses.map((house) => (
-          <div key={house._id} style={{ marginRight: "20px", marginBottom: "20px", width: "200px", boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)", borderRadius: "10px", overflow: "hidden" }}>
-            <Link to={`/HouseDetails/${house._id}`} style={{ textDecoration: "none", color: "#333" }}>
-              <img src={`http://localhost:3000/${house.images}`} style={{ width: "100%", height: "auto" }} />
-              <div style={{ padding: "15px" }}>
-                <h3 style={{ fontSize: "18px", fontWeight: "bold", marginBottom: "10px" }}>{house.title}</h3>
-                <p style={{ marginBottom: "10px", display: "inline-flex", alignItems: "center" }}>
-                  <span style={{ marginRight: "5px", color: "#F27438" }}>
-                    <FaLocationDot />
-                  </span>
-                  {house.wilaya}
-                </p>
-              </div>
-            </Link>
-          </div>
-        ))}
+    <div style={{ backgroundColor: '#f0f0f0', minHeight: '100vh' }}>
+      <Navbar />
+      <div style={{ fontFamily: "Arial, sans-serif" }}>
+        <h1 style={{ fontSize: "36px", fontWeight: "bold", textAlign: "center", marginBottom: "40px" }}>Résultats de la recherche</h1>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexWrap: "wrap" }}>
+          {houses.map((house) => (
+            <div key={house._id} style={{ marginRight: "20px", marginBottom: "20px", width: "350px", height: "400px", boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)", borderRadius: "10px", overflow: "hidden", position: "relative" }}>
+              <Link to={`/HouseDetails/${house._id}`} style={{ textDecoration: "none", color: "#333" }}>
+                <div style={{ height: "250px", overflow: "hidden" }}>
+                  {house.images && house.images.length > 0 ? (
+                    <img src={house.images[0].url} style={{ width: "100%", height: "100%", objectFit: "cover" }} alt="House" />
+                  ) : (
+                    <img src={sorry} style={{ width: "100%", height: "100%", objectFit: "cover" }} alt="No Image Available" />
+                  )}
+                </div>
+                <div style={{ padding: "15px" }}>
+                  <h3 style={{ fontSize: "18px", fontWeight: "bold", marginBottom: "10px" }}>{house.title}</h3>
+                  <p style={{ marginBottom: "10px", display: "inline-flex", alignItems: "center" }}>
+                    <span style={{ marginRight: "5px", color: "#F27438" }}>
+                      <FaLocationDot />
+                    </span>
+                    {house.wilaya}
+                  </p>
+                  <p>{house.price} DA</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <Link to="/home" style={{ color: '#F27438', textDecoration: 'none', fontWeight: 'bold' }}>Retour à la page d'accueil</Link>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
